d58c49ec9cfbbf2511c24ffd2f1742bebe50526e
Denis Denisov <denis.denisov8701@gmail.com>
2024-05-04 13:51:27 +0200

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Denis Denisov on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
